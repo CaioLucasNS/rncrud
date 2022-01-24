1fec8acf667254923ff672bccc58ad961e483190
Caio Lucas <lcaio1281@gmail.com>
2022-01-23 22:40:57 -0300

    add mock users

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'josil@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/07/03/10/54/naruto-6383641_1280.png'
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: 'mariagui@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2022/01/04/08/23/woman-6914576_1280.png'
+    },
+    {
+        id: 3,
+        name: 'Roberto Fragoso',
+        email: 'robfra@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/01/18/08/32/naruto-5927441_1280.png'
+    },
+    {
+        id: 4,
+        name: 'Rafael Monteiro',
+        email: 'rafamonte@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_1280.png'
+    },
+]
