f41911ab2437ae8a480fb12fcb41e961d805471a
Caio Lucas <lcaio1281@gmail.com>
2022-01-23 23:19:05 -0300

    feat UserList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import UserList from './views/UserForm';
+import UserList from './views/UserList';
 import UserForm from './views/UserForm';
 import { Button, Icon } from 'react-native-elements';
-// import { Icon } from 'react-native-vector-icons/Icon';
 
 const Stack = createNativeStackNavigator();
 
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,9 +1,66 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, FlatList, Text, Alert } from 'react-native';
+import { ListItem, Avatar, Button, Icon } from 'react-native-elements';
+import users from '../data/users';
 
-function UserList() {
+function UserList({ navigation }) {
+
+    function confirmUserDelection(user) {
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress() {
+                    console.warn(`Delete ${user.id} - ${user.name}`)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getAction(user) {
+        return (
+            <>
+                <Button 
+                    onPress={() => navigation.navigate('UserForm', user)}
+                    type='clear'
+                    icon={<Icon name='edit' size={25} color='orange' />}
+                />
+                <Button 
+                    onPress={() => confirmUserDelection(user)}
+                    type='clear'
+                    icon={<Icon name='delete' size={25} color='red' />}
+                />
+            </>
+        )
+    }
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                key={user.id}
+                bottomDivider
+                onPress={() => navigation.navigate('UserForm', user)}
+            >
+                <Avatar source={{ uri: user.avatarUrl }} />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+                {getAction(user)}
+            </ListItem>
+        )
+    }
+
     return(
-        <Text>UserList</Text>
+        <View>
+            <FlatList 
+                keyExtractor={user => user.id}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
 
