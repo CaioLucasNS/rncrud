48ed373188caf93bf587d6fe9e71266efdbe9aff
Caio Lucas <lcaio1281@gmail.com>
2022-01-23 22:09:16 -0300

    fix linking react-native-vector-icons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import UserList from './views/UserForm';
 import UserForm from './views/UserForm';
+import { Button, Icon } from 'react-native-elements';
+// import { Icon } from 'react-native-vector-icons/Icon';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,18 +13,47 @@ const App = () => {
         <NavigationContainer>
             <Stack.Navigator
                 initialRouteName='UserList'
+                screenOptions={screenOptions}
             >
                 <Stack.Screen 
                     name='UserList'
                     component={UserList}
+                    options={
+                        () => {
+                            return {
+                                title: 'Lista de Usuários',
+                                headerRight: () => {
+                                    return(
+                                        <Button 
+                                            type='clear'
+                                            icon={<Icon name='add' size={25} color='white' />}
+                                        />
+                                    );
+                                }
+                            }
+                        }
+                    }
                 />
                 <Stack.Screen 
                     name='UserForm'
                     component={UserForm}
+                    options={{
+                        title: 'Formulário de Usuários'
+                    }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
-    )
+    );
+};
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold',
+    }
 }
 
 export default App;
